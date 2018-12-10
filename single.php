6592a7eb072bcad8e32e3e4b53868878682a3f67
Bryan Leal <bryan.ls019@gmail.com>
2018-12-10 02:10:35 -0600

    spaces removed

--- single.php
+++ single.php
@@ -1,23 +1,12 @@
 <?php get_header(); ?>
 
 <div id="main-container">
-
     <div class="main-content single-article">
-
+    
         <!-- <div class="tree-cats"><?php //echo get_the_category_list( ' / ', 'multiple' ); ?></div> -->
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-        <article class="clearfix">
-            <h1><?php the_title(); ?></h1>
-            <div class="date-info">
-                <p class="date-entry">Por <span class="author-entry"><?php the_author(); ?></span> | <?php the_time('j F, Y') ?> a las <?php the_time(); ?></p>
-            </div>
-            <div class="post-cats"><?php the_category(', '); ?></div>
-            <div class="thumb">
-                <?php if ( has_post_thumbnail() ) { the_post_thumbnail ( 'vs-big-thumb' ); } ?>
-            </div>
-            <div class="extract"><?php the_content(); ?></div>
-        </article>
-        <?php endwhile; else: ?>
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); 
+            get_template_part('content', get_post_format());
+        endwhile; else: ?>
         <p>No se encontraron artículos.</p>
         <?php endif; wp_reset_query(); ?>
 
@@ -25,7 +14,6 @@
 
     </div>
     <?php get_sidebar(); ?>
-
 </div>
 
 <?php get_footer(); ?>
