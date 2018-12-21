9a3061778762678c71dc9e92a3123f02a759fc0b
Bryan Leal <bryan.ls019@gmail.com>
2018-12-20 18:20:28 -0600

    Remove Unnecessary code lines

--- js/main_nav.js
+++ js/main_nav.js
@@ -1,22 +1,12 @@
 $(document).ready(function() {
     var altura = $(".primary-navigation").offset().top;
-    //var altura2 = $('.menu-bar').offset().top;
-    //var ancho = screen.width;
-    //var menu_size = $(".mob-menu-content").height();
 
     $(window).on('scroll', function() {
-        //if (($(window).scrollTop() > altura2) && (screen.width <= 640)) {
-            //$('.menu-bar').addClass('menu-fixed');
-            //$('.primary-navigation').addClass('tmenu-fixed');
-        //} else {
-            //$('.menu-bar').removeClass('menu-fixed');
-            //$('.primary-navigation').removeClass('tmenu-fixed');
             if (($(window).scrollTop() > altura)) {
                 $('.primary-navigation').addClass('menu-fixed');
             } else {
                 $('.primary-navigation').removeClass('menu-fixed');
             }
-        //}
     });
 
     $("#btn-open").click(function() {
